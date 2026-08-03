29a5bebf1494a51346194aa9a1c601f911883a57
Maneeka03 <joshineeharika981@gmail.com>
2026-08-03 18:47:55 +0530

    package.json modified

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -27,6 +27,8 @@ const nextConfig: NextConfig = {
     maxInactiveAge: 25 * 1000,
     pagesBufferLength: 2,
   },
+
+  allowedDevOrigins: ["keyword-scenic-velvet-wheel.trycloudflare.com"],
 };
 
 export default nextConfig;
